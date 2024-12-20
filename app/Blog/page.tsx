8375b2275c0ad62550bc583e9dc31db062ddb695
Muhammad Sharjeel <m.sharjeelasif1435@gmail.com>
2024-12-20 11:18:24 -0800

    .

--- app/Blog/page.tsx
+++ app/Blog/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import BgPic from "@/app/Components/BgPic";
 import Header from "../Components/Header"
 import BlogCard from "../Components/BlogComponent/BlogCard"
-import Pagination from '../Components/ShopComponents/Pagination';
 
 const Blog = () => {
   return (
